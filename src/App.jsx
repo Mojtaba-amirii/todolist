a72b01dd0cdec95b4a7d0089a325e6e5aa880fcf
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-02-23 14:16:44 +0100

    Delete Button done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 let count = 1;
 
 function App() {
-  const [list, setList] = useState([{ title: "Diska", done: false, id: 0 }]);
+  const [list, setList] = useState([{ title: "", done: false, id: 0 }]);
   const [text, setText] = useState();
 
   function handleChange(event) {
@@ -17,10 +17,8 @@ function App() {
       done: false,
       id: count++,
     };
-
     const newList = [...list];
     newList.push(todo);
-
     setList(newList);
   }
 
@@ -37,10 +35,24 @@ function App() {
     setList(newList);
   }
 
-  function handleDelete() {}
+  function handleDelete(id) {
+    const newList = [...list];
+
+    for (let i = 0; i < newList.length; i++) {
+      const item = newList[i];
+      if (item.id == id) {
+        newList.splice(i, 1);
+      }
+    }
+
+    setList(newList);
+  }
 
   return (
-    <div className="App">
+    <div>
+      <header>
+        <h1>Todo List</h1>
+      </header>
       <input type="text" onChange={handleChange} />
       <button onClick={handleAdd}>Add</button>
 
@@ -51,7 +63,9 @@ function App() {
             <button onClick={() => handleDone(item.id, item.done)}>
               {item.done ? "Undo" : "Done"}
             </button>
-            <button>Delete</button>
+            <button onClick={() => handleDelete(item.id, item.done)}>
+              Delete
+            </button>
           </div>
         );
       })}
